a0bab1ab72539d10c056f5f3c81adb15ad369361
Jesse Pollak <jpollak92@gmail.com>
2012-02-08 11:03:12 -0800

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IndependentStudy::Application.config.secret_token = '85e7cc8060140d96273dd7e3d2c8068da782904252d9f8e6262bce034d717dc7a5e91bd9852f0e961f93643e419074adde21aa72de93a2f57a14caf9eaa32075'
