fdba53a258c534d37f5ce78c02a39e4bfb642ee3
Jesse Pollak <jpollak92@gmail.com>
2012-02-15 11:24:48 -0800

    log in and log out

--- features/step_definitions/submit_request_steps.rb
+++ features/step_definitions/submit_request_steps.rb
@@ -2,14 +2,14 @@ Given /^I am a logged in user$/ do
   visit "/auth/facebook"
 end
 
+Given /^I am on the new request page$/ do
+  visit new_request_url
+end
+
 Then /^I am taken to the show page for that request$/ do
   response.should have_selector('.request_title'), content: @title
 end
 
 Then /^I am returned to the form$/ do
   response.should have_selector('title', content: 'New Request')
-end
-
-Then /^a notice is displayed saying the title was blank$/ do
-  request.should have_selector('.flash', content: "Please submit a valid title.")
 end
