7cd575adfcb963ca70c3f95c367a68ef9a8d3297
Kimberly Merrill <kmerrill@cs227-13.cs.pomona.edu>
2012-02-20 12:20:17 -0800

    completed rspec

--- spec/views/requests/edit.html.erb_spec.rb
+++ spec/views/requests/edit.html.erb_spec.rb
@@ -4,6 +4,7 @@ describe "requests/edit" do
   before(:each) do
     @request = assign(:request, stub_model(Request,
       :title => "MyString",
+      :date => Time.now + 3.day,
       :description => "MyText"
     ))
   end
@@ -15,6 +16,8 @@ describe "requests/edit" do
     assert_select "form", :action => requests_path(@request), :method => "post" do
       assert_select "input#request_title", :name => "request[title]"
       assert_select "textarea#request_description", :name => "request[description]"
+      assert_select "label", :content => "Date"
+      assert_select "input", :type => "Submit"
     end
   end
 end
--- spec/views/requests/new.html.erb_spec.rb
+++ spec/views/requests/new.html.erb_spec.rb
@@ -4,6 +4,7 @@ describe "requests/new" do
   before(:each) do
     assign(:request, stub_model(Request,
       :title => "MyString",
+      :date => Time.now + 3.day,
       :description => "MyText"
     ).as_new_record)
   end
@@ -15,6 +16,8 @@ describe "requests/new" do
     assert_select "form", :action => requests_path, :method => "post" do
       assert_select "input#request_title", :name => "request[title]"
       assert_select "textarea#request_description", :name => "request[description]"
+      assert_select "label", :content => "Date"
+      assert_select "input", :type => "Submit"
     end
   end
 end
