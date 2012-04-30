f323116410952180f9e180cdaa74d8bf317baf3f
Jesse Pollak <jpollak92@gmail.com>
2012-04-30 11:31:48 -0700

    all of cucumber testing is now working

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,5 @@
 class Message < ActiveRecord::Base
-	validates_presence_of :name, :email, :message, :reward
-	validates_length_of :reward, :maximum => 20
+	validates_presence_of :name, :email, :message
 	validates_uniqueness_of :message, :scope => :name
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 	validates_format_of :phone, :with => /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/, :allow_blank => true
